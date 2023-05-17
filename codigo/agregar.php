8f6a7bfc302a3bf95a243f988e6b2209b76a66d5
Pablo <pablodominguezvalle@gmail.com>
2023-05-17 16:25:05 +0200

    Solucion de errores

---
+++ codigo/agregar.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<title>Bestiario Online</title>
+	</head>
+	<body>
+				<h1>Socios</h1>
+							<!-- Categoría -->
+							<label><p>Lugar</p>
+							<select name="categoria" id="" >
+								<option value="amateur">Amateur</option>
+								<option value="profesional">Profesional</option>
+							</select></label>
+							<!-- Registrar -->
+							<input type="submit" value="Guardar" id="" class="btn btn-primary">
+	</body>
+</html>	
