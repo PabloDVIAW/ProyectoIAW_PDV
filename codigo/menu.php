1944625d24e805e3a8e5769fa7b6572d3d6c7fb1
Pablo <pablodominguezvalle@gmail.com>
2023-05-17 15:44:59 +0200

    Resolucion de problemas

--- codigo/menu.php
+++ codigo/menu.php
@@ -23,7 +23,38 @@
     <main>
         <h2>Estos son los avistamientos registrados</h2>
         <!-- tabla de los avistamientos recientes -->
-        <!-- la tabla dispondrá de 4 campos: nombre de la criatura, lugar de avistamiento, hora del registro y una entrada a informacion de la criatura -->
+        
+        <table>
+				<thead>
+					<tr>
+						<th>Nombre Monstruo</th>
+						<th>Zona</th>
+						<th>Fecha ocurrida</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+
+						 while($fila = $resultado->fetch_assoc()){
+							
+							echo "<tr>";
+							echo "<td>$fila[nom_mon]</td>";
+							echo "<td>$fila[zona]</td>";
+							echo "<td>$fila[fecha]</td>";
+							echo "</tr>";
+						}
+						
+					?>
+				</tbody>
+			</table>
+
+			<div>
+				<!-- Registrar -->
+				<p><a href="agregar.php" name="btn">Registrar</a></p>
+			</div>
+        <!-- la tabla dispondrá de 3 campos: nombre de la criatura, lugar de avistamiento y hora del registro-->
         <!-- el fonbdo constará de un estampado de escamas verdes -->
 
 
