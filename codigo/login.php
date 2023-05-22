b78c04dcfedc963e1f7eb1e816631bb258b3e915
Pablo <pablodominguezvalle@gmail.com>
2023-05-22 15:52:59 +0200

    estilos finales

--- codigo/login.php
+++ codigo/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bestiario Online</title>
 </head>
-<body style="background-image: url('../imagenes/fondogremio.jpg');" >
+<body style="background-image: url('../imagenes/fondogremio.jpg');">
 <div class="container">
     <header>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -32,7 +32,7 @@
     </div>
     </main>
     <footer>
-        
+
     </footer>
 </div>
 </body>
