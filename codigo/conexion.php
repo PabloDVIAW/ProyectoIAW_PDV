1d092cd51d312be33f6f17741735a8757a7869f5
Pablo <pablodominguezvalle@gmail.com>
2023-06-15 18:19:25 +0200

    oldbackup

--- codigo/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$mysqli = new mysqli("localhost", "root", "", "bestiario");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectarm a MySQL: (", $mysqli->connect_errno, ")", $mysqli->connect_error;
-}
