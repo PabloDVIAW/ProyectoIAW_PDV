995dc7f5b21f05dfeb5703ed037e9e39647d6b1d
Pablo <pablodominguezvalle@gmail.com>
2023-06-15 13:48:41 +0200

    especies(tipos)agregar y eliminar

--- codigo/especies.php
+++ codigo/especies.php
@@ -1,7 +1,7 @@
 <?php
 
 	require('conexion.php');
-	$sql="SELECT * FROM avistamientos";
+	$sql="SELECT * FROM tipos";
 	$resultado=$mysqli->query($sql)
 	
 ?>
@@ -15,30 +15,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bestiario Online</title>
-
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container">
-<header class="menucab">
+    <header>
 	<div class="container">
     <h1 class="display-1">Bienvenido al bestiario online</h1>
 	</div>
-
     </header>
     <main>
-		<div class="container">
-        <h2>Estos son los avistamientos registrados</h2>
-        
+        <div class="container">
+		<h2>Estas son las especies registradas</h2>
         <table class="table">
 				<thead style="background-color: black; color: white;">
 					<tr>
-						<th scope="col">Nombre Monstruo</th>
-						<th scope="col">Zona</th>
-						<th scope="col">Fecha ocurrida</th>
-						<th></th>
-						<th></th>
+						<th scope="col">Especie</th>
+						<th scope="col">Debilidad</th>
+						<th scope="col">Fortaleza</th>
+						<th scope="col">Definicion</th>
+						<th scope="col">Arma</th>
 					</tr>
 				</thead>
 				<tbody style="background-color: grey;">
@@ -47,23 +43,32 @@
 						 while($fila = $resultado->fetch_assoc()){
 							
 							echo "<tr>";
-							echo "<td scope='row'>$fila[nom_mon]</td>";
-							echo "<td scope='row'>$fila[zona]</td>";
-							echo "<td scope='row'>$fila[fecha]</td>";
+							echo "<td scope='row'>$fila[especie]</td>";
+							echo "<td scope='row'>$fila[debilidad]</td>";
+							echo "<td scope='row'>$fila[fortaleza]</td>";
+							echo "<td scope='row'>$fila[definicion]</td>";
+                            echo "<td scope='row'>$fila[arma]</td>";
+							echo "<td scope='row'><a href='ed_esp.php?id=$fila[id]'><button type='button' class='btn btn-primary'>Editar</button></a></td>";
+							echo "<td scope='row'><a href='del_esp.php?id=$fila[id]'><button type='button' class='btn btn-primary'>Eliminar</button></a></td>";
 							echo "</tr>";
 						}
 						
 					?>
 				</tbody>
 			</table>
-		</div>
-			<div class="container d-flex justify-content-between bd-highlight mb-3">
-					<a href="login.php" name="btn"><button type="button" class="btn btn-primary">Volver</button></a>
-					<p class="col-sm"></p>
-					<a href="tipos.php" name="btn"><button type="button" class="btn btn-primary">Ver la tabla informativa</button></a>
+			</div>
+
+			<div  class="container d-flex justify-content-between bd-highlight mb-3">
+				
+				<p><a href="reg_esp.php" name="btn"><button type="button" class="btn btn-primary">Agregar</button></a></p>
+			
+				<p><a href="login.php" name="btn"><button type="button" class="btn btn-primary">Volver</button></a></p>
+				
+				<p><a href="tipos.php" name="btn"><button type="button" class="btn btn-primary">Ver la tabla informativa</button></a></p>
+				
 			</div>
     </main>
-	</div>
+    <footer>
+    </footer>
 </body>
-
 </html>
---
+++ codigo/reg_esp.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="es">
+	<head>
+	<link rel="stylesheet" type="text/css" href="../estilos/micss.css">
+		<title>Bestiario Online</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+	<?php
+	?>
+	<div class="container">
+				<h1>Añade una especie</h1>
+                <br>
+						<form action="reg_esp2.php" method="GET">
+							<label for=""><p>Nombre de especie: <input type="text" name="nombre" required></p></label>
+							<br>
+                            <label for=""><p>Descripción: <input type="text" name="descrip" maxlength="50" required></p></label>
+                            <br>
+                            <label><p>Elige una debilidad porfavor</p>
+							<select name="debilidad" id="" >
+								<option selected value="Fuego">Fuego</option>
+								<option value="Hielos">Hielo</option>
+								<option value="Dragon">Agua</option>
+								<option value="Viento">Viento</option>
+							</select>
+                            </label>
+                            <br>
+                            <label><p>Elige una fortaleza porfavor</p>
+							<select name="fortaleza" id="" >
+								<option selected value="Fuego">Fuego</option>
+								<option value="Hielos">Hielo</option>
+								<option value="Dragon">Agua</option>
+								<option value="Viento">Viento</option>
+							</select>
+                            </label>
+                            <br>
+                            <label><p>Elige un arma</p>
+							<select name="arma" id="" >
+								<option value="Fuego">Espada</option>
+								<option selected value="Hielos">Espadalarga</option>
+								<option value="Dragon">Martillo</option>
+								<option value="Sangrado">Glaive</option>
+							</select>
+                            </label>
+							<br>
+							<input type="submit" value="Registrar">
+							<br>
+							<a href='especies.php' class='btn btn-primary'>Regresar</a>
+						</form>
+	</div>
+	</body>
+</html>	
